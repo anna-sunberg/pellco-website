6ddf2d6bdda7a6af0f21855a74219056b1174c7a
Anna <anna@osipova.fi>
2016-09-15 18:38:31 +0300

    added description and keywords meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="fi">
 <head>
-    <title>Pellco</title>
+    <title>PellCompany</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pellettien tilaus ja kotiintoimitus">
+    <meta name="keywords" content="pellcompany,pellco,pelletti,puupelletti,pelletit,pellettitilaus,pellet,pellets">
 
     <!-- CSS And JavaScript -->
     {!! Html::style('css/app.css') !!}
