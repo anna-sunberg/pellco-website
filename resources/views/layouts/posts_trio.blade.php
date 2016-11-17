926f0a4b826679d67135d06bd693c73330423833
Anna Osipova <anna.osipova@cgi.com>
2016-11-17 14:27:39 +0200

    temporarily hardcoded text in trio post

--- resources/views/layouts/posts_trio.blade.php
+++ resources/views/layouts/posts_trio.blade.php
@@ -1,8 +1,11 @@
 <h1>{{ $trio['title'] }}</h1>
-<div class="col-sm-4 sub-block">
+<!-- <div class="col-sm-4 sub-block">
     <div class="col-sm-8 col-sm-offset-2">
         <img class="img-responsive" src="/img/{{ $trio['image'] }}" />
     </div>
 </div>
 <div class="col-sm-4 sub-block">{{ strip_tags($trio['text_1']) }}</div>
-<div class="col-sm-4 sub-block">{{ strip_tags($trio['text_2']) }}</div>
+<div class="col-sm-4 sub-block">{{ strip_tags($trio['text_2']) }}</div> -->
+<div class="col-sm-4 sub-block">PellCompany Oy tarjoaa asiakkailleen vain puupuruista valmistettuja pellettejä.</div>
+<div class="col-sm-4 sub-block">Ostamme puupuruja mekaaniselta puunjalostusteollisuudelta, joka käyttää Venäjän pohjoisalueiden puhdasta kuivattua ympäristöystävällistä puuraaka-ainetta. Se antaa mahdollisuuden optimoida tuotantoa ja tuottaa ympäristöystävällisiä, laadukkaita ja edullisia pellettejä.</div>
+<div class="col-sm-4 sub-block">Tilatkaa kotiinkuljetus ja vakuuttukaa pellettimme korkeasta laadusta!</div>
